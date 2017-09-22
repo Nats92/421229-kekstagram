e5b0e5b74562e88c21052666bda380e1399a5e80
Olga Popova <30652104+Nats92@users.noreply.github.com>
2017-09-22 15:00:25 +0500

    Внесла поправку

--- js/photo-load.js
+++ js/photo-load.js
@@ -1,22 +1,25 @@
 'use strict';
 
-var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
-var photoPreview = document.querySelector('.effect-image-preview');
+(function () {
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+  var photoPreview = document.querySelector('.effect-image-preview');
 
-window.uploadFile.addEventListener('change', function () {
-  // debugger
-  var file = window.uploadFile.files[0];
-  var fileName = file.name.toLowerCase();
+  window.uploadFile.addEventListener('change', function () {
+    // debugger
+    var file = window.uploadFile.files[0];
+    var fileName = file.name.toLowerCase();
 
-  var matches = FILE_TYPES.some(function (it) {
-    return fileName.endsWith(it);
-  });
-
-  if (matches) {
-    var reader = new FileReader();
-    reader.addEventListener('load', function () {
-      photoPreview.src = reader.result;
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
     });
-    reader.readAsDataURL(file);
-  }
-});
+
+    if (matches) {
+      var reader = new FileReader();
+      reader.addEventListener('load', function () {
+        photoPreview.src = reader.result;
+      });
+      reader.readAsDataURL(file);
+    }
+  });
+})();
+
